d95b667b12efe6621297654f884334750d7b0313
Matthew Markic <matt.markic@hotmail.com>
2016-10-05 17:02:06 +0000

    Returns the indices of letters within a string

---
+++ indices.js
@@ -0,0 +1,34 @@
+function letterIndices(myString) {
+  var myObject = {};
+  for (index in myString) {
+    var letter = myString[index];
+
+    if (letter !== " ") {
+
+      if (myObject[letter]) {
+        myObject[letter].push(Number(index));
+      }
+      else {
+        myObject[letter] = [Number(index)];
+      }
+    }
+  }
+  return myObject;
+}
+
+console.log(letterIndices("lighthouse in the house"));
+
+
+// Output should be
+// {
+//   l: [0],
+//   i: [1, 11],
+//   g: [2],
+//   h: [3, 5, 15, 18],
+//   t: [4, 14],
+//   o: [6, 19],
+//   u: [7, 20],
+//   s: [8, 21],
+//   e: [9, 16, 22],
+//   n: [12]
+// }
